8563b01baed9985e5bdff3dc45d676f498cb219e
Luca Di Leo <admin@lucadileo.it>
2023-04-26 19:04:39 +0200

    Removed model from repository

--- evaluation/run.sh
+++ evaluation/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Download the latest release of the OPC repository
-#wget https://github.com/uav4geo/OpenPointClass/releases/download/latest/opc.tar.gz
-wget https://digipa.it/wp-content/uploads/opc.tar.gz
+latest_release_url=$(curl -s "https://api.github.com/repos/uav4geo/OpenPointClass/releases/latest" | jq -r '.assets[0].browser_download_url')
+wget $latest_release_url -O opc.tar.gz
 tar -xzvf opc.tar.gz
 chmod +x pctrain pcclassify
 
@@ -23,8 +23,6 @@ echo "RADIUS: $RADIUS"
 # Step 4: Execute pctrain on all point clouds from step 2 with the settings from step 3
 ./pctrain --classifier gbt --scales $SCALES --resolution $RESOLUTION --radius $RADIUS --output new-model.bin $DATASET_FILES
 
-# Checkout the ground truth repository is done in the workflow.yml
-
 # Get the list of all point cloud file paths in the ground truth repository
 GROUND_TRUTH_FILES=$(find ground-truth -type f -iname "*.laz" -o -iname "*.las" -o -iname "*.ply")
 
@@ -41,10 +39,3 @@ for FILE in $GROUND_TRUTH_FILES; do
 
   ./pcclassify --regularization local_smooth --reg-radius $RADIUS --eval --stats-file $NEW_STATS_FILE $FILE $OUTPUT_FILE new-model.bin
 done
-
-# Add new-model.bin to the PR
-git config --global user.email "noreply@github.com"
-git config --global user.name "GitHub Actions"
-git add new-model.bin
-git commit -m "Add new-model.bin"
-git push origin HEAD:$GITHUB_HEAD_REF

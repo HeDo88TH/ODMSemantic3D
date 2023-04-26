11bcba35b5a8a628f5772fb9ffe6b50208cf74ab
Luca Di Leo <admin@lucadileo.it>
2023-04-26 10:16:20 +0200

    Fix new-stats.json path

--- evaluation/run.sh
+++ evaluation/run.sh
@@ -32,8 +32,14 @@ echo "Ground Truth Files: $GROUND_TRUTH_FILES"
 
 # Step 7: Execute pcclassify for each point cloud from step 6 with the new-model.bin from step 4 and stats-file new-stats.json
 for FILE in $GROUND_TRUTH_FILES; do
+
   OUTPUT_FILE="${FILE%.*}_classified.${FILE##*.}"
-  ./pcclassify --regularization local_smooth --reg-radius $RADIUS --eval --stats-file new-stats.json $FILE $OUTPUT_FILE new-model.bin
+
+  # Calculate the new-stats.json file path
+  FOLDER=$(dirname $FILE)
+  NEW_STATS_FILE="${FOLDER}/new-stats.json"
+
+  ./pcclassify --regularization local_smooth --reg-radius $RADIUS --eval --stats-file $NEW_STATS_FILE $FILE $OUTPUT_FILE new-model.bin
 done
 
 # Step 8: Get the list of all new-stats.json file paths created in step 7 and existing stats.json file paths
